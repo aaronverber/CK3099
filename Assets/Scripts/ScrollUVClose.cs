08261e06163890cc835553dd4b0186ea08079359
aaronverber <aaron.verber@gmail.com>
2015-01-11 23:51:01 -0800

    finished csharp conversion

---
+++ Assets/Scripts/ScrollUVClose.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollUVClose : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		Material mat = this.renderer.material;
+		Vector2 offset = mat.mainTextureOffset;
+
+		offset.x = transform.position.x / 200;
+		offset.y = transform.position.y / 200;
+		
+		mat.mainTextureOffset = -(offset);
+	}
+}
---
+++ Assets/Scripts/ScrollUVFar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollUVFar : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		Material mat = this.renderer.material;
+		Vector2 offset = mat.mainTextureOffset;
+
+		offset.x = transform.position.x / 300;
+		offset.y = transform.position.y / 300;
+		
+		mat.mainTextureOffset = offset;
+	}
+}
