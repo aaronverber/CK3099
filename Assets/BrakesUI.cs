52a62da0c96c2eeee135b7a40c12d4a2323ed02b
aaronverber <aaron.verber@gmail.com>
2015-01-12 22:11:13 -0800

    planet hitbox, UI testing

---
+++ Assets/BrakesUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrakesUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		animation.Play ("BrakesFlash");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Fire1")){
+			renderer.enabled = true;
+		}
+		else{
+			renderer.enabled = false;
+		};
+	}
+}
