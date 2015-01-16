34ffc342612c06c2c3f48726dcca9f73b9804f82
aaronverber <aaron.verber@gmail.com>
2015-01-15 23:06:37 -0800

    started ship blink on hit

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -2,7 +2,12 @@
 using System.Collections;
 
 public class EnemyBehavior : MonoBehaviour {
+
+	public Color transparent = new Color (1f, 1f, 1f, 0.5f);
+
 	void OnTriggerEnter2D(Collider2D other){
+		Destroy(other.gameObject);
+		this.GetComponent<SpriteRenderer>().color = transparent;
 		Debug.Log ("hit!");
 	}
 }	
