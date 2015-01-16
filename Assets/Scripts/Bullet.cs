928213bc22f905e73672f1a1c7cd2ecf372337fb
aaronverber <aaron.verber@gmail.com>
2015-01-15 22:27:01 -0800

    implemented autofire at target

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,21 +6,26 @@ public class Bullet : MonoBehaviour {
 	public float thrust = 10;
 	private GameObject player;
 	private PlayerMovement playerMovement;
+	private TargetEnemies te;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Player");
-		playerMovement = GameObject.Find ("Player").GetComponent<PlayerMovement>();
+		playerMovement = player.GetComponent<PlayerMovement>();
+		te = player.GetComponent<TargetEnemies> ();
 
-		float radRotation = playerMovement.radRotation;
-		float playerVelX = player.rigidbody2D.velocity.x;
-		float playerVelY = player.rigidbody2D.velocity.y;
+		Vector2 dir = (te.selectedTarget.transform.position - transform.position).normalized * thrust;
+		//dir = te.selectedTarget.InverseTransformDirection (dir);
+		//float angle = Mathf.Atan2(dir.y, dir.x);
+
+		//float playerVelX = player.rigidbody2D.velocity.x;
+		//float playerVelY = player.rigidbody2D.velocity.y;
 		
-		float vx = -(Mathf.Sin(radRotation) * thrust) + playerVelX;
-		float vy = (Mathf.Cos(radRotation) * thrust) + playerVelY;
+		//float vx = (Mathf.Sin(angle) * thrust) + playerVelX;
+		//float vy = (Mathf.Cos(angle) * thrust) + playerVelY;
 		
-		Vector2 bulletVelocity = new Vector2(vx, vy);
-		rigidbody2D.velocity = bulletVelocity;
+		//Vector2 bulletVelocity = new Vector2(vx, vy);
+		rigidbody2D.velocity = dir;
 	
 	}
 	
--- Assets/Scripts/FireForward.cs
+++ Assets/Scripts/FireForward.cs
@@ -4,12 +4,30 @@ using System.Collections;
 public class FireForward : MonoBehaviour {
 
 	public GameObject bullet;
-		
+	TargetEnemies te;
+	float distanceToEnemy;
+
+	void Start() {
+		te = gameObject.GetComponent<TargetEnemies>();
+	}
+
 	// Update is called once per frame
+
 	void Update () {
-		if(Input.GetKeyDown("up")){
-			Instantiate(bullet, transform.position, transform.rotation);
-			Physics2D.IgnoreCollision(bullet.collider2D, collider2D);
+		if (te.selectedTarget == null) {
+			return;
+		} else {
+
+			distanceToEnemy = Vector2.Distance (transform.position, te.selectedTarget.transform.position);
 		}
+		if (distanceToEnemy <= 5.0) {
+			FireAtTarget ();
+		}
+	}
+
+	void FireAtTarget(){
+
+		Instantiate(bullet, transform.position, transform.rotation);
+		Physics2D.IgnoreCollision(bullet.collider2D, collider2D);
 	}
 }
