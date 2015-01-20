1c325c7c2c95e4d4f372d4ad7019f7d3dec81320
aaronverber <aaron.verber@gmail.com>
2015-01-19 23:23:43 -0800

    started shield recharge

--- Assets/Scripts/ShieldBar.cs
+++ Assets/Scripts/ShieldBar.cs
@@ -9,11 +9,30 @@ public class ShieldBar : MonoBehaviour {
 	private float hueShiftBase;
 	private float hueShiftTotal;
 	private float satShiftBase;
+	bool chargeNow;
+	float rechargeCylce;
 
 	void Start(){
 		shieldHealth = totalShieldHealth;
 	}
 
+	void Update(){
+		if (shieldHealth >= totalShieldHealth) {
+			return;
+		} else {
+			if (chargeNow == true) {
+				rechargeShieldHealth (1);
+			}
+			rechargeCylce += Time.deltaTime;
+			if (rechargeCylce >= 0.4f) {
+				chargeNow = true;
+				rechargeCylce = 0.0f;
+			} else {
+				chargeNow = false;
+			}
+		}
+	}
+	
 	void OnTriggerEnter2D(Collider2D other){
 		damageDealt = other.gameObject.GetComponent<Bullet> ().damageDealt;
 		if (other.gameObject.tag == "FriendlyProjectile") {
@@ -22,18 +41,27 @@ public class ShieldBar : MonoBehaviour {
 		}
 	}
 
-	void drainShieldHealth(float damage){
-		hueShiftBase = (damage / totalShieldHealth) * 150;
+	void shiftShieldColor (float amount){
+		hueShiftBase = (amount / totalShieldHealth) * 150;
 		satShiftBase = 2;
+		hueShiftTotal = hueShiftBase * ((totalShieldHealth - shieldHealth) / amount);
+		Debug.Log (hueShiftTotal);
+		renderer.material.SetFloat("_HueShift", hueShiftTotal);
+		renderer.material.SetFloat("_Sat", satShiftBase);
+	}
+
+	void drainShieldHealth(float damage){
 		if (shieldHealth > 1) {
 			shieldHealth -= damage;
-			hueShiftTotal = hueShiftBase * ((totalShieldHealth - shieldHealth) / damage);
-			Debug.Log (hueShiftTotal);
-			renderer.material.SetFloat("_HueShift", hueShiftTotal);
-			renderer.material.SetFloat("_Sat", satShiftBase);
+			shiftShieldColor(damage);
 		}
 		else{
 			Destroy (gameObject);
 		}
+	}
+
+	void rechargeShieldHealth(float rechargeRate){
+		shieldHealth += rechargeRate;
+		shiftShieldColor(-rechargeRate);
 	}
 }
