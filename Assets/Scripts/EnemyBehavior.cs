83a30dde67c0b8e6a65afb9d0c136dcba6f1f7f0
aaronverber <aaron.verber@gmail.com>
2015-01-22 17:32:30 -0800

    shields spawn correctly

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,7 +4,16 @@ using System.Collections;
 public class EnemyBehavior : MonoBehaviour {
 
 	public int hullHealth = 20;
+	public GameObject shieldArray;
 	public Color transparent = new Color (1f, 1f, 1f, 0.5f);
+	int[] rotationArray = new int[] { 0, 60, 120, 180, 240, 300};
+
+	void Start(){
+		foreach(int shieldRotation in rotationArray){
+			GameObject newShield = Instantiate(shieldArray, transform.position, Quaternion.Euler(0, 0, shieldRotation)) as GameObject;
+			newShield.transform.parent = gameObject.transform;
+		}
+	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		if ((other.gameObject.tag == "FriendlyProjectile") && (hullHealth > 1)){
--- Assets/Scripts/ShieldArray.cs
+++ Assets/Scripts/ShieldArray.cs
@@ -20,27 +20,39 @@ public class ShieldArray : MonoBehaviour {
 		foreach (ShieldBar shield in shieldBars) {
 			if (shield.gameObject.name == "innerShieldBar"){
 			    if(shield.shieldHealth >= shield.totalShieldHealth) {
-
-					if (canActivate == true) {
-						setOuterShieldArrayActive ();
-					}
-					activationCycle += Time.deltaTime;
-					if (activationCycle >= 0.1f) {
-						canActivate = true;
-						activationCycle = 0.0f;
-					} else {
-						canActivate = false;
-					}
+					runShieldActivationCycle();
+				}
+				if(shield.shieldHealth == null){
+					runShieldActivationCycle();
 				}
 			}
 		}
 	}
-	void setOuterShieldArrayActive(){
+
+	void setShieldArrayActive(){
 		foreach (ShieldBar shield in shieldBars) {
 			if(shield.gameObject.name == "outerShieldBar"){
 				shield.gameObject.SetActive(true);
 				Debug.Log ("success");
-			}		
+			}
+			if(shield.gameObject.name == "innerShieldBar"){
+				if(shield.gameObject.activeInHierarchy == false){
+					shield.gameObject.SetActive(true);
+				}
+			}
+		}
+	}
+
+	void runShieldActivationCycle(){
+		if (canActivate == true) {
+			setShieldArrayActive ();
+		}
+		activationCycle += Time.deltaTime;
+		if (activationCycle >= 1.5f) {
+			canActivate = true;
+			activationCycle = 0.0f;
+		} else {
+			canActivate = false;
 		}
 	}
 }
