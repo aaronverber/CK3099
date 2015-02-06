5d4dcc9d705bbe96f74299d76611d51477ad7f42
aaronverber <aaron.verber@gmail.com>
2015-02-05 20:20:58 -0800

    update

---
+++ Assets/Managers/MessageAreaUIManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageAreaUIManager : MonoBehaviour {
+
+	public GameObject speakerTextUI;
+	public GameObject eventTextUI;
+
+	// Use this for initialization
+	void Start () {
+		GameEventManager.EnemyDestroyed += EnemyDestroyed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void EnemyDestroyed(){
+		//Instantiate(speakerTextUI, transform.position, transform.rotation);
+	}
+}
